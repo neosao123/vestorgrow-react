020847fc325e8acca38eda9d08deca65a3f398dc
Admin <neosao123>
2023-09-09 15:38:37 +0530

    adjust top position of chattype

--- src/pages/home/ChatsType.js
+++ src/pages/home/ChatsType.js
@@ -69,7 +69,7 @@ function ChatsType() {
 
   return (
     <>
-      <div className="feedChatBox stickyChatBox" style={{ position: "fixed", right: 0, top: `${atTop ? "6vh" : bottom}` }} >
+      <div className="feedChatBox stickyChatBox" style={{ position: "fixed", right: 0, top: `${atTop ? "4.4em" : bottom}` }} >
         <div className="feedChatContent" style={{ height: "92%", width: "18.6em", borderRadius: "15px 15px 0 0", boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }} >
           <div className="tab-content">
             {(user.role.includes("admin") || user.role.includes("userPaid")) && (
