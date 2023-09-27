6630fe09307fc26ba4adc5fd30151ae92b2b5236
Admin <neosao123>
2023-09-27 11:57:31 +0530

    add

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -3,27 +3,25 @@ import "./news.css"
 import { IoIosArrowForward } from "react-icons/io"
 import { VscChevronLeft, VscChevronRight } from "react-icons/vsc"
 import NewsComp from './NewsComp'
-const NewsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const NewsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const tabs = ["VestorGrow", "Economics", "Politics", "Bussiness", "Maharashatra", "India", "Kolhapur", "Technology", "Health", "Automotive"]
 
 function News() {
-
+  const [page, setPage] = useState(1)
+  const [activeTab, setActiveTab] = useState("all")
 
   return (
     <div className='new_box'>
       <div className='main_news_box'>
         <p className='news-heading'>VestorGrow News</p>
         <div className='tabs_box' >
-          <div className='news_tabs'>
-            <div className='news_tabs_text active_tab'>VestorGrow</div>
-            <div className='news_tabs_text'>Economics</div>
-            <div className='news_tabs_text'>Politics</div>
-            <div className='news_tabs_text'>Bussiness</div>
-            <div className='news_tabs_text'>Maharashatra</div>
-            <div className='news_tabs_text'>India</div>
-            <div className='news_tabs_text'>Kolhapur</div>
-            <div className='news_tabs_text'>Technology</div>
-            <div className='news_tabs_text'>Health</div>
-            <div className='news_tabs_text'>Automotive</div>
+          <div className='news_tabs' id="carousel">
+            <div className={`news_tabs_text ${activeTab === "all" && "active_tab"}`} onClick={() => setActiveTab("all")}>All</div>
+            {
+              tabs.length > 0 && tabs.map((el, i) => {
+                return <div className={`news_tabs_text item ${activeTab === el && "active_tab"}`} onClick={() => setActiveTab(el)}>{el}</div>
+              })
+            }
           </div>
           <div>
             <button className='forward_btn'>
@@ -40,11 +38,11 @@ function News() {
           }
         </div>
         <div className='grid_pagination'>
-          <button>
+          <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
             <VscChevronLeft />
           </button>
-          <button><span className='page_name'>1</span>/4</button>
-          <button>
+          <button><span className='page_name'>{page}</span>/4</button>
+          <button disabled={page >= 4} onClick={() => setPage(page + 1)}>
             <VscChevronRight />
           </button>
         </div>
